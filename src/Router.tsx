f597cfa42e335f4092766dae44ace45c6782b7c1
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-07-11 16:51:50 -0300

    default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,17 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import DefaultLayout from './layouts/DefaultLayout'
 import Home from './pages/Home'
 import History from './pages/History'
-import React from 'react'
 
 const Router: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
