387e532a793a888aea1b4fcacd0472ce12399962
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-07-12 10:15:11 -0300

    adjusting theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components'
-import { theme } from './themes/theme'
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -10,12 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
-    background: ${theme['gray-900']};
-    color: ${theme['gray-300']};
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input-security, textarea, button {
